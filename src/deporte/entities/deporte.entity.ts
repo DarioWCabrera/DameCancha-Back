95a736f3d036bb98344e4ccdbe7a89e6352d207e
DarioWCabrera <dariowcabrera@gmail.com>
2026-08-28 17:19:58 -0300

    "borrado de clubes"

--- src/deporte/entities/deporte.entity.ts
+++ src/deporte/entities/deporte.entity.ts
@@ -1,17 +1,31 @@
-import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  OneToMany,
+} from 'typeorm';
+
 import { Cancha } from '../../cancha/entities/cancha.entity';
 
 @Entity('deporte')
 export class Deporte {
   @PrimaryGeneratedColumn({ name: 'id_deporte' })
-  id_deporte: number;
+  id_deporte!: number;
 
-  @Column({ name: 'nombre_deporte', type: 'varchar', length: 100 })
-  nombre_deporte: string;
+  @Column({
+    name: 'nombre_deporte',
+    type: 'varchar',
+    length: 100,
+  })
+  nombre_deporte!: string;
 
-  @Column({ name: 'descripcion_deporte', type: 'text', nullable: true })
-  descripcion_deporte: string;
+  @Column({
+    name: 'descripcion_deporte',
+    type: 'text',
+    nullable: true,
+  })
+  descripcion_deporte!: string | null;
 
   @OneToMany(() => Cancha, (cancha) => cancha.id_deporte)
-  canchas: Cancha[];
+  canchas!: Cancha[];
 }
