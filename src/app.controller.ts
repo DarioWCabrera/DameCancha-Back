091f64c76e338ed36c3122b15664114d7e269f97
DarioWCabrera <dariowcabrera@gmail.com>
2026-09-14 11:42:24 -0300

    reduce database background activity

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -14,4 +14,9 @@ export class AppController {
   getHealth() {
     return this.appService.getHealth();
   }
+
+  @Get('health/db')
+  getDatabaseHealth() {
+    return this.appService.getDatabaseHealth();
+  }
 }
